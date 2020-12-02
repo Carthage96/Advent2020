6d524e1f133f3dc12d5c0cb7c6c8c3bcea2b27e2
Adam Dormier <adam@recroom.com>
2020-12-02 04:42:44 -0800

    Actually finish puzzle 1 - didn't realize there was a Part 2 initially

--- 1/src/Puzzle1.java
+++ 1/src/Puzzle1.java
@@ -7,8 +7,7 @@ import java.util.Scanner;
 public class Puzzle1 {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner console = new Scanner(System.in);
-        System.out.print("Input File: ");
-        Scanner input = new Scanner(new File(console.next()));
+        Scanner input = new Scanner(new File(CommonUtils.prompt(console, "Input File: ")));
         console.close();
 
         List<Integer> list = new ArrayList<>(200);
@@ -17,6 +16,7 @@ public class Puzzle1 {
         }
         input.close();
 
+        System.out.println("===== Part 1 =====");
         boolean done = false;
         for (int i = 0; i < list.size() && !done; i++) {
             for (int j = i+1; j < list.size() && !done; j++) {
@@ -24,6 +24,26 @@ public class Puzzle1 {
                     System.out.println(list.get(i) + " + " + list.get(j) + " = 2020");
                     System.out.println(list.get(i) + " * " + list.get(j) + " = " + list.get(i) * list.get(j));
                     done = true;
+                }
+            }
+        }
+
+        System.out.println("===== Part 2 =====");
+        done = false;
+        for (int i = 0; i <list.size() && !done; i++) {
+            int num1 = list.get(i);
+            for (int j = i+1; j < list.size() && !done; j++) {
+                int num2 = list.get(j);
+                if (num1 + num2 > 2020) {
+                    continue;
+                }
+                for (int k = j+1; k < list.size() && !done; k++) {
+                    int num3 = list.get(k);
+                    if (num1 + num2 + num3 == 2020) {
+                        System.out.printf("%d + %d + %d = 2020%n", num1, num2, num3);
+                        System.out.printf("%d * %d * %d = %d%n", num1, num2, num3, num1 * num2 * num3);
+                        done = true;
+                    }
                 }
             }
         }
