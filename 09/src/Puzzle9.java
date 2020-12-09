cfe92d67c20ae942d1b2f5b36f466f234ce1753c
Adam Dormier <adam@recroom.com>
2020-12-09 01:05:36 -0800

    Solve Puzzle9 part 1
    Use a LinkedList to record valid sums as we traverse the list of the data only once.
    Each time we move forward, pull off the first 25 elements of the list - should be O(n) for n size of preamble (not data!) - and then append the newly generated 25.
    
    Slower to search for a value though, so that's the tradeoff

--- 09/src/Puzzle9.java
+++ 09/src/Puzzle9.java
@@ -1,5 +1,25 @@
-public class Puzzle9 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Puzzle9 {
+    public static void main(String[] args) throws IOException {
+        Scanner console = new Scanner(System.in);
+        Scanner input = new Scanner(new File(CommonUtils.prompt(console, "input file: ")));
+        console.close();
+
+        List<Integer> data = new ArrayList<>(1000);
+        while (input.hasNextInt()) {
+            data.add(input.nextInt());
+        }
+        input.close();
+
+        XMASData xmas = new XMASData(data, 25);
+
+        CommonUtils.printPartHeader(1);
+
+        System.out.printf("First invalid entry: %d%n", xmas.findFirstInvalid());
     }
 }
---
+++ 09/src/XMASData.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class XMASData {
+    private final List<Integer> data;
+    private final int preambleSize;
+
+    public XMASData(List<Integer> data, int preambleSize) {
+        this.data = List.copyOf(data);
+        this.preambleSize = preambleSize;
+    }
+
+    public int findFirstInvalid() {
+        List<Integer> validSums = new LinkedList<>();
+        // Prime list for preamble
+        for (int i = 0; i < preambleSize; i++) {
+            int value1 = data.get(i);
+            for (int j = 0; j < i; j++) {
+                int value2 = data.get(j);
+                validSums.add(value1 != value2 ? value1 + value2 : null);
+            }
+            for (int j = 0; j < preambleSize - i; j++) {
+                validSums.add(null);
+            }
+        }
+
+        for (int i = preambleSize; i < data.size(); i++) {
+            int newValue = data.get(i);
+            if (!validSums.contains(newValue)) {
+                return newValue;
+            }
+            validSums.subList(0, preambleSize).clear(); // Learned new thing!
+            for (int j = i - preambleSize; j < i; j++) {
+                validSums.add(newValue + data.get(j));
+            }
+        }
+        throw new IllegalStateException("Searched data, but didn't find invalid entry!");
+    }
+}
