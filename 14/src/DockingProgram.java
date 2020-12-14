56f079ee727c1bfbb3b21efc73c4e494b8a062e1
Adam Dormier <adam@recroom.com>
2020-12-14 01:49:22 -0800

    Solve Puzzle14 part 2

--- 14/src/DockingProgram.java
+++ 14/src/DockingProgram.java
@@ -5,23 +5,49 @@ public class DockingProgram {
     private String rawMask;
     private long inverseMask;
     private long positiveMask;
-    private final Map<Integer, Long> memory;
+    private long xMask;
+    private final Map<Long, Long> memory;
 
     public DockingProgram() {
         rawMask = "0";
         inverseMask = (long)Math.pow(2, 37) - 1;
         positiveMask = 0;
+        xMask = 0;
         memory = new HashMap<>();
     }
 
-    public void setMask(String mask) {
+    public void setMask(String mask, boolean useV2) {
         rawMask = mask;
         positiveMask = Long.parseLong(rawMask.replace('X', '0'), 2);
-        inverseMask = Long.parseLong(rawMask.replace('1', '0').replace('X', '1'), 2);
+        if (useV2) {
+            inverseMask = Long.parseLong(rawMask.replace('1', 'X').replace('0', '1').replace('X', '0'), 2);
+            xMask = Long.parseLong(rawMask.replace('1', '0').replace('X', '1'), 2);
+        } else {
+            inverseMask = Long.parseLong(rawMask.replace('1', '0').replace('X', '1'), 2);
+        }
     }
 
-    public void writeMemory(int address, long value) {
-        memory.put(address, (value & inverseMask) + positiveMask);
+    public void writeMemory(long address, long value, boolean useV2) {
+        if (useV2) {
+            address &= inverseMask;
+            address += positiveMask;
+            writeFloating(address, xMask, value);
+        } else {
+            memory.put(address, (value & inverseMask) + positiveMask);
+        }
+    }
+
+    private void writeFloating(long baseAddress, long floatingMask, long value) {
+        if (floatingMask == 0) {
+            memory.put(baseAddress, value);
+        } else {
+            long floatingBit = 1;
+            while ((floatingBit & floatingMask) == 0 && floatingBit < Math.pow(2, 37)) {
+                floatingBit *= 2;
+            }
+            writeFloating(baseAddress, floatingMask ^ floatingBit, value);
+            writeFloating(baseAddress | floatingBit, floatingMask ^ floatingBit, value);
+        }
     }
 
     public long getSum() {
--- 14/src/Puzzle14.java
+++ 14/src/Puzzle14.java
@@ -10,18 +10,28 @@ public class Puzzle14 {
         CommonUtils.printPartHeader(1);
 
         DockingProgram program = new DockingProgram();
+        executeInput(program, input, false);
+        System.out.printf("Sum of values in memory = %d%n%n", program.getSum());
+
+        CommonUtils.printPartHeader(2);
+
+        program = new DockingProgram();
+        executeInput(program, input, true);
+        System.out.printf("Sum of values in memory = %d%n%n", program.getSum());
+    }
+
+    public static void executeInput(DockingProgram program, List<String> input, boolean useV2) {
         Pattern memPattern = Pattern.compile("mem\\[(?<address>\\d+)] = (?<value>\\d+)");
         for (String line : input) {
             if (line.startsWith("mask")) {
-                program.setMask(line.split(" ")[2]);
+                program.setMask(line.split(" ")[2], useV2);
             } else {
                 Matcher m = memPattern.matcher(line);
                 if (!m.matches()) {
                     throw new IllegalArgumentException("Invalid line in input: " + line);
                 }
-                program.writeMemory(Integer.parseInt(m.group("address")), Long.parseLong(m.group("value")));
+                program.writeMemory(Integer.parseInt(m.group("address")), Long.parseLong(m.group("value")), useV2);
             }
         }
-        System.out.printf("Sum of values in memory = %d%n%n", program.getSum());
     }
 }
