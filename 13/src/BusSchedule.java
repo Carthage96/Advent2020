e7ee9afafffbdff76d61d0326cab423bbbc7bb2c
Adam Dormier <adam@recroom.com>
2020-12-13 17:35:54 -0800

    Solve Puzzle13 part 2

--- 13/src/BusSchedule.java
+++ 13/src/BusSchedule.java
@@ -1,24 +1,69 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class BusSchedule {
-    private final List<Integer> buses;
+    private final Map<Integer, Integer> buses;
+    private final List<Integer> busesSorted;
 
-    public BusSchedule(List<Integer> buses) {
-        this.buses = new ArrayList<>(buses.size());
-        this.buses.addAll(buses);
+    public BusSchedule(String[] buses) {
+        this.buses = new HashMap<>(buses.length);
+        this.busesSorted = new ArrayList<>(buses.length);
+        for (int i = 0; i < buses.length; i++) {
+            try {
+                int bus = Integer.parseInt(buses[i]);
+                this.buses.put(bus, i);
+                this.busesSorted.add(bus);
+            } catch (NumberFormatException ignored) {}
+        }
+        Collections.sort(busesSorted);
+        Collections.reverse(busesSorted);
     }
 
     public int busWaitProduct(int minDepartTime) {
-        List<Integer> waitTimes = buses.stream().map(bus -> waitTime(bus, minDepartTime)).collect(Collectors.toList());
+        List<Integer> waitTimes = busesSorted.stream().map(bus -> waitTime(bus, minDepartTime)).collect(Collectors.toList());
         int minWait = Collections.min(waitTimes);
-        int bus = buses.get(waitTimes.indexOf(minWait));
+        int bus = busesSorted.get(waitTimes.indexOf(minWait));
         return bus * minWait;
     }
 
     private static int waitTime(int schedule, int minDepartTime) {
         return (schedule - (minDepartTime % schedule));
+    }
+
+    public long magicTimestamp() {
+        Iterator<Integer> itr = busesSorted.iterator();
+        int x = itr.next();
+        int y = itr.next();
+        long firstSuccess = (y * firstNToMatch(x, y, buses.get(y) - buses.get(x), 0)) - buses.get(y);
+        long lcm = lcm(x, y);
+        while (itr.hasNext()) {
+            x = itr.next();
+            firstSuccess += lcm * firstNToMatch(x, lcm, -buses.get(x), firstSuccess);
+            lcm = lcm(lcm, x);
+        }
+        return firstSuccess;
+    }
+
+    private static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    private static long lcm(long a, long b) {
+        return (a / gcd(a, b)) * b;
+    }
+
+    // offset is of y, relative to x
+    // returns n such that
+    // d + n*y = c + m*x
+    // for some integer m
+    private static long firstNToMatch(long x, long y, long c, long d) {
+        long n = 1;
+        while ((d + n * y - c) % x != 0) {
+            n++;
+        }
+        return n;
     }
 }
--- 13/src/Puzzle13.java
+++ 13/src/Puzzle13.java
@@ -1,9 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Puzzle13 {
     public static void main(String[] args) throws FileNotFoundException {
@@ -13,14 +10,15 @@ public class Puzzle13 {
 
         int timeEstimate = Integer.parseInt(input.nextLine());
         String unparsedSchedule = input.nextLine();
-        List<Integer> allBuses = Arrays.stream(unparsedSchedule.split(","))
-                .filter(s -> !s.equals("x"))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        String[] allBuses = unparsedSchedule.split(",");
         BusSchedule busSchedule = new BusSchedule(allBuses);
 
         CommonUtils.printPartHeader(1);
 
         System.out.printf("Earliest departure product: %d%n%n", busSchedule.busWaitProduct(timeEstimate));
+
+        CommonUtils.printPartHeader(2);
+
+        System.out.printf("Magic timestamp: %d%n", busSchedule.magicTimestamp());
     }
 }
