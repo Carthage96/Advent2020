22696a3136ffd8acfc69326222210743c611dfe8
Adam Dormier <adam@recroom.com>
2020-12-18 02:02:48 -0800

    Solve Puzzle18 part 2

--- 18/src/Puzzle18.java
+++ 18/src/Puzzle18.java
@@ -11,16 +11,28 @@ public class Puzzle18 {
 
         CommonUtils.printPartHeader(1);
 
-        long sum = input.stream().mapToLong(Puzzle18::compute).sum();
+        long sum = input.stream().mapToLong(s -> compute(s, true)).sum();
         System.out.printf("Sum = %d%n%n", sum);
+
+        CommonUtils.printPartHeader(2);
+        sum = input.stream().mapToLong(s -> compute(s, false)).sum();
+        System.out.printf("Sum = %d%n", sum);
     }
 
-    public static long compute(String equation) {
+    public static long compute(String equation, boolean equalPrecedence) {
         Pattern parenthesesPattern = Pattern.compile("\\(([^()]+)\\)");
         Matcher m = parenthesesPattern.matcher(equation);
         while (m.find()) {
-            equation = m.replaceAll(Puzzle18::parensReplace);
+            equation = m.replaceAll(equalPrecedence ? Puzzle18::parensReplaceEqualPrecedence : Puzzle18::parensReplaceUnqualPrecedence);
             m.reset(equation);
+        }
+        if (!equalPrecedence) {
+            Pattern additionPattern = Pattern.compile("(\\d+) \\+ (\\d+)");
+            m = additionPattern.matcher(equation);
+            while (m.find()) {
+                equation = m.replaceAll(Puzzle18::additionReplace);
+                m.reset(equation);
+            }
         }
         Scanner eqScan = new Scanner(equation);
         long first = eqScan.nextInt();
@@ -35,7 +47,15 @@ public class Puzzle18 {
         return first;
     }
 
-    public static String parensReplace(MatchResult match) {
-        return "" + compute(match.group(1));
+    public static String parensReplaceEqualPrecedence(MatchResult match) {
+        return "" + compute(match.group(1), true);
+    }
+
+    public static String parensReplaceUnqualPrecedence(MatchResult match) {
+        return "" + compute(match.group(1), false);
+    }
+
+    public static String additionReplace(MatchResult match) {
+        return "" + (Long.parseLong(match.group(1)) + Long.parseLong(match.group(2)));
     }
 }
