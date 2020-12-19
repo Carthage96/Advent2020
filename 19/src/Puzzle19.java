fe40dfa3ca7db5e1847a180736526d00edeb08b3
Adam Dormier <adam@recroom.com>
2020-12-19 15:17:19 -0800

    Solve Puzzle19 part 2

--- 19/src/Puzzle19.java
+++ 19/src/Puzzle19.java
@@ -3,12 +3,16 @@ import java.util.List;
 
 public class Puzzle19 {
     public static void main(String[] args) throws IOException {
-        List<String> input = CommonUtils.readInputToList();
-        int split = input.indexOf("");
-        MonsterMessageMatcher matcher = new MonsterMessageMatcher(input.subList(0, split));
-
-        CommonUtils.printPartHeader(1);
-        long matchCount = input.subList(split + 1, input.size()).stream().filter(s -> matcher.match(s, 0)).count();
-        System.out.printf("Number of rule 0 matches: %d%n%n", matchCount);
+        for (int i = 1; i <= 2; i++) {
+            CommonUtils.printPartHeader(1);
+            List<String> input = CommonUtils.readInputToList();
+            int split = input.indexOf("");
+            MonsterMessageMatcher matcher = new MonsterMessageMatcher(input.subList(0, split));
+            long matchCount = input.subList(split + 1, input.size()).stream().filter(s -> matcher.match(s, 0)).count();
+            System.out.printf("Number of rule 0 matches: %d%n", matchCount);
+            if (i == 1) {
+                System.out.println();
+            }
+        }
     }
 }
