ce44c8f4c43841c2c4deb584ed27aaa4268788e6
Adam Dormier <adam@recroom.com>
2020-12-22 01:20:40 -0800

    Solve Puzzle22 part 2

--- 22/src/CombatManager.java
+++ 22/src/CombatManager.java
@@ -1,37 +1,30 @@
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class CombatManager {
     private final Queue<Integer> deck1;
     private final Queue<Integer> deck2;
+    private final Set<GameState> visitedStates;
 
-    public CombatManager(List<String> deck1, List<String> deck2) {
-        this.deck1 = new LinkedList<>();
-        this.deck2 = new LinkedList<>();
-        for (String card : deck1) {
-            this.deck1.add(Integer.parseInt(card));
-        }
-        for (String card : deck2) {
-            this.deck2.add(Integer.parseInt(card));
-        }
+    public CombatManager(List<Integer> deck1, List<Integer> deck2) {
+        this.deck1 = new LinkedList<>(deck1);
+        this.deck2 = new LinkedList<>(deck2);
+        visitedStates = new HashSet<>();
     }
 
     private boolean isGameOver() {
         return deck1.isEmpty() || deck2.isEmpty();
     }
 
-    public int playToEnd() {
+    public int playStandardCombat() {
         int count = 0;
         while (!isGameOver()) {
-            play1Round();
+            play1RoundStandard();
             count++;
         }
         return count;
     }
 
-    private void play1Round() {
+    private void play1RoundStandard() {
         if (isGameOver()) {
             throw new IllegalStateException("Game is over!");
         }
@@ -45,6 +38,40 @@ public class CombatManager {
         }
     }
 
+    // Returns true if player 1 wins the game, false if player 2 wins the game
+    public boolean playRecursiveCombat() {
+        while (!isGameOver()) {
+            GameState currentState = new GameState(deck1, deck2);
+            if (visitedStates.contains(currentState)) {
+                return true;
+            }
+            visitedStates.add(currentState);
+            play1RoundRecursive();
+        }
+        return deck2.isEmpty();
+    }
+
+    private void play1RoundRecursive() {
+        int card1 = deck1.remove();
+        int card2 = deck2.remove();
+        boolean player1Wins;
+        if (deck1.size() >= card1 && deck2.size() >= card2) {
+            CombatManager recursiveManager = new CombatManager(
+                    ((LinkedList<Integer>)deck1).subList(0, card1),
+                    ((LinkedList<Integer>)deck2).subList(0, card2));
+            player1Wins = recursiveManager.playRecursiveCombat();
+        } else {
+            player1Wins = card1 > card2;
+        }
+        if (player1Wins) {
+            deck1.add(card1);
+            deck1.add(card2);
+        } else {
+            deck2.add(card2);
+            deck2.add(card1);
+        }
+    }
+
     public long winnerScore() {
         Iterator<Integer> itr = (deck1.isEmpty() ? deck2 : deck1).iterator();
         long multiplier = deck1.isEmpty() ? deck2.size() : deck1.size();
@@ -54,5 +81,29 @@ public class CombatManager {
             multiplier--;
         }
         return total;
+    }
+
+    private static class GameState {
+        private final Queue<Integer> deck1;
+        private final Queue<Integer> deck2;
+
+        public GameState(Queue<Integer> deck1, Queue<Integer> deck2) {
+            this.deck1 = new LinkedList<>(deck1);
+            this.deck2 = new LinkedList<>(deck2);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            GameState gameState = (GameState) o;
+            return deck1.equals(gameState.deck1) &&
+                    deck2.equals(gameState.deck2);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(deck1, deck2);
+        }
     }
 }
--- 22/src/Puzzle22.java
+++ 22/src/Puzzle22.java
@@ -1,17 +1,23 @@
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Puzzle22 {
     public static void main(String[] args) throws IOException {
         List<String> input = CommonUtils.readInputToList();
-        List<String> deck1 = input.subList(1, input.indexOf(""));
-        List<String> deck2 = input.subList(input.indexOf("") + 2, input.size());
-
-        CombatManager combatManager = new CombatManager(deck1, deck2);
+        List<Integer> deck1 = input.subList(1, input.indexOf("")).stream().map(Integer::parseInt).collect(Collectors.toList());
+        List<Integer> deck2 = input.subList(input.indexOf("") + 2, input.size()).stream().map(Integer::parseInt).collect(Collectors.toList());
 
         CommonUtils.printPartHeader(1);
 
-        int rounds = combatManager.playToEnd();
+        CombatManager combatManager = new CombatManager(deck1, deck2);
+        int rounds = combatManager.playStandardCombat();
         System.out.printf("Game took %d rounds.%nWinner score: %d%n%n", rounds, combatManager.winnerScore());
+
+        CommonUtils.printPartHeader(2);
+
+        combatManager = new CombatManager(deck1, deck2);
+        combatManager.playRecursiveCombat();
+        System.out.printf("Winner score: %d%n", combatManager.winnerScore());
     }
 }
